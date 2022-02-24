d716b00c6ea728d51382d434725ba7aed5c80c17
fxpby <fxpby@outlook.com>
2022-02-25 00:29:32 +0800

    feat: update exercise typeof-guard

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import everydayTypes from './everyday-types/index' // 基础类型
+import typeofGuard from './typeof-guard/index' // 类型守卫
 
-everydayTypes()
+// everydayTypes()
+typeofGuard()
--- src/typeof-guard/index.ts
+++ src/typeof-guard/index.ts
@@ -1,18 +1,39 @@
-function padLeft(padding: number | string, input: string): string {
-  if (typeof padding === 'number') {
-    return new Array(padding + 1).join(' ') + input
-  } else {
-    return padding + input
+export default function () {
+  function padLeft(padding: number | string, input: string): string {
+    if (typeof padding === 'number') {
+      return new Array(padding + 1).join(' ') + input
+    } else {
+      return padding + input
+    }
   }
-}
 
-function printAll(strs: string | string[] | null) {
-  if (typeof strs === 'object') {
-    // 会报错，因为 typeof null === 'object'
-    // strs.forEach(s => {
-    //   console.log(s)
-    // })
-  } else if (typeof strs === 'string') {
-  } else {
+  function printAll(strs: string | string[] | null) {
+    if (strs && typeof strs === 'object') {
+      strs.forEach(s => {
+        console.log(s)
+      })
+    } else if (typeof strs === 'string') {
+      console.log(strs.concat('hello'))
+    } else {
+      // ...
+    }
   }
+
+  function getUserOnlineMessage(numUsersOnline: number) {
+    if (numUsersOnline) {
+      return `现在共有 ${numUsersOnline} 人在线`
+    }
+    return `现在没有人在线`
+  }
+
+  function multiplyAll(values: number[] | undefined, factor: number) {
+    if (!values) {
+      return values
+    } else {
+      return values.map(x => {
+        return x * factor
+      })
+    }
+  }
+  console.log(multiplyAll([2, 3, 4], 3))
 }
